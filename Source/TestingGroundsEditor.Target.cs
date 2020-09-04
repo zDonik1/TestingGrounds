270610b1c03336a6ccdb85dad9b6ff04da998a8e
zDonik <tokhirovdoniyor@gmail.com>
2020-09-04 12:43:29 +0500

    Initial commit

---
+++ Source/TestingGroundsEditor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class TestingGroundsEditorTarget : TargetRules
+{
+	public TestingGroundsEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.Add("TestingGrounds");
+	}
+}
